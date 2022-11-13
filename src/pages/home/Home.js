8dfe37dad11b0abf1cc1ff149f71d36fa19cb3d5
Christian Obinna Iheacho <iheachochristian@yahoo.com>
2022-11-13 23:45:18 +0000

    cards designs

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../home/Home.css"
 import { Col, Container, Row } from 'react-bootstrap'
-import { FaDeezer, FaAccessibleIcon } from "react-icons/fa"
+import { FaDeezer, FaAccessibleIcon, FaAddressCard, FaAddressBook, FaWpforms, FaBlenderPhone, FaExpandArrowsAlt } from "react-icons/fa"
 
 const Home = () => {
   return (
@@ -12,13 +12,55 @@ const Home = () => {
                     <ul className='list'>
                         <li><FaDeezer size={22} />&nbsp;Stats</li>
                         <li><FaAccessibleIcon size={22} />&nbsp;All Jobs</li>
-                        <li>Add Job</li>
-                        <li>Profile</li>
+                        <li><FaAddressCard size={22} />&nbsp;Add Job</li>
+                        <li><FaAddressBook size={22} />&nbsp;Profile</li>
                     </ul>
                 </div>
             </Col>
             <Col md={10} className="content-col">
-
+                <div className='cards'>
+                    <Row>
+                        <Col md={4}>
+                            <div className='card-control'>
+                                <div className='card-1'>
+                                    <div className='faP1'>
+                                        <h6 style={{color: "rgb(254, 235, 208)", fontSize: 26}}>21</h6>
+                                    </div>
+                                    <div className='faP'>
+                                        <FaWpforms className='FawpformsIcon' />
+                                    </div>
+                                </div>
+                                <h5>Pending Application</h5>
+                            </div>
+                        </Col>
+                        <Col md={4}>
+                            <div className='card-control'>
+                                    <div className='card-1'>
+                                        <div className='faP1'>
+                                            <h6 style={{color: "rgb(254, 235, 208)", fontSize: 26}}>50</h6>
+                                        </div>
+                                        <div className='faP'>
+                                            <FaBlenderPhone className='FawpformsIcon' />
+                                        </div>
+                                    </div>
+                                    <h5>Interviews Scheduled</h5>
+                            </div>
+                        </Col>
+                        <Col md={4}>
+                        <div className='card-control'>
+                            <div className='card-1'>
+                                <div className='faP1'>
+                                    <h6 style={{color: "rgb(254, 235, 208)", fontSize: 26}}>8</h6>
+                                </div>
+                                <div className='faP'>
+                                    <FaExpandArrowsAlt className='FawpformsIcon' />
+                                </div>
+                                </div>
+                                <h5>Jobs Declined</h5>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </Col>
         </Row>
     </Container>
